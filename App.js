506b59ca6bbae1e8fcbc18f9b28e7bf994280447
KhrystynaKo <postforimportantthings@gmail.com>
2020-06-10 00:00:59 +0300

    created navigation in a separate file

--- App.js
+++ App.js
@@ -1,32 +1,6 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Icon from "react-native-vector-icons/FontAwesome";
+import Navigation from "./Navigation";
 
-import CocktailsListScreen from "./screens/CocktailsListScreen";
-import FilterListScreen from "./screens/FilterListScreen";
-
-const Stack = createStackNavigator();
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Drinks'
-          component={CocktailsListScreen}
-          options={({ navigation }) => ({
-            headerRight: () => (
-              <Icon
-                onPress={() => navigation.navigate("Filters")}
-                name='filter'
-                size={35}
-                color='black'
-              />
-            ),
-          })}
-        />
-        <Stack.Screen name='Filters' component={FilterListScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
